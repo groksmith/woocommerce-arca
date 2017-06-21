6e44c4d161d90ad179d93a8586bcd0e4d78f9949
Nairi Abgaryan <nairi.abgaryan@gmail.com>
2017-06-21 19:37:32 +0400

    [FIX](Arca): Added payment handle callback method

--- wc-arca.php
+++ wc-arca.php
@@ -9,33 +9,33 @@ Author URI: http://www.hexdivision.com/
 */
 
 // Include our Gateway Class and Register Payment Gateway with WooCommerce
-add_action( 'plugins_loaded', 'woocommerce_arca_init', 0 );
-function woocommerce_arca_init() {
-	// If the parent WC_Payment_Gateway class doesn't exist
-	// it means WooCommerce is not installed on the site
-	// so do nothing
-	if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
+add_action('plugins_loaded', 'woocommerce_arca_init', 0);
 
-	// If we made it this far, then include our Gateway Class
-	include_once( 'wc-arca-gateway.php' );
+function woocommerce_arca_init()
+{
+    if (!class_exists('WC_Payment_Gateway')) return;
 
-	// Now that we have successfully included our class,
-	// Lets add it too WooCommerce
-	add_filter( 'woocommerce_payment_gateways', 'add_arca_gateway' );
-	function add_arca_gateway( $methods ) {
-		$methods[] = 'WC_ArCa';
-		return $methods;
-	}
+    include_once('wc-arca-gateway.php');
+
+    add_filter('woocommerce_payment_gateways', 'add_arca_gateway');
+
+    /**
+     * Add the gateway to WooCommerce
+     **/
+    function add_arca_gateway($methods)
+    {
+        $methods[] = 'WC_ArCa';
+        return $methods;
+    }
+
 }
 
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'woocommerce_arca_action_links');
+function woocommerce_arca_action_links($links)
+{
+    $plugin_links = array(
+        '<a href="' . admin_url('admin.php?page=wc-settings&tab=checkout') . '">' . __('Settings', 'arca') . '</a>',
+    );
 
-// Add custom action links
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'woocommerce_arca_action_links' );
-function woocommerce_arca_action_links( $links ) {
-	$plugin_links = array(
-		'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout' ) . '">' . __( 'Settings', 'arca' ) . '</a>',
-	);
-
-	// Merge our new link with the default ones
-	return array_merge( $plugin_links, $links );
+    return array_merge($plugin_links, $links);
 }
